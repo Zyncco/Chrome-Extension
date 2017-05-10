eb357989bd4d2c72f34fc7c92505ee66120bd3b6
Brandon (Yeti) <branicyeti@live.co.uk>
2017-05-10 15:38:28 +0100

    Asset retrieval helper function

--- util.js
+++ util.js
@@ -2,6 +2,10 @@ const ZyncUtil = {}
 
 ZyncUtil.isSignedIn = function() {}
 
+ZyncUtil.getAsset = function(path) {
+	return chrome.runtime.getUrl("/assets/" + path);
+}
+
 ZyncUtil.handleCallbackError = function() {
 	if (chrome.runtime.lastError) {
 		console.error(chrome.runtime.lastError.message);
