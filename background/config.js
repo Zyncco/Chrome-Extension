4a7ae0d7840ad0452d715aa27aeceb62ef2a6da6
Amir Omidi <aaomidi@users.noreply.github.com>
2017-05-11 12:35:24 -0400

    Fix callback code.

--- background/config.js
+++ background/config.js
@@ -1,44 +1,45 @@
 function handleError() {
-	if (chrome.runtime.lastError) {
-		// TODO: Properly display error to user
-		console.error(chrome.runtime.lastError.message);
-	}
+    if (chrome.runtime.lastError) {
+        // TODO: Properly display error to user
+        console.error(chrome.runtime.lastError.message);
+    }
 }
 
 function initializeConfig(clear) {
-	if (clear) {
-		chrome.storage.local.clear(handleError);
-	}
+    if (clear) {
+        chrome.storage.local.clear(handleError);
+    }
 
-	chrome.storage.local.set({
-		/* Epoch of initialization */
-		"initialized": parseInt(new Date().getTime() / 1000),
+    chrome.storage.local.set({
+        /* Epoch of initialization */
+        "initialized": parseInt(new Date().getTime() / 1000),
 
-		/* REST API URL */
-		"api.url.scheme": "https",
-		"api.url.domain": "beta-api.zync.co",
-		"api.url.version": 0,
-		"api.url.format": "{scheme}://{domain}/v{version}/{endpoint}",
+        /* REST API URL */
+        "api.url.scheme": "https",
+        "api.url.domain": "beta-api.zync.co",
+        "api.url.version": 0,
+        "api.url.format": "{scheme}://{domain}/v{version}/{endpoint}",
 
-		/* REST API Auth */
-		"api.auth.header": "X-ZYNC-TOKEN",
-		"api.auth.token": null,
+        /* REST API Auth */
+        "api.auth.header": "X-ZYNC-TOKEN",
+        "api.auth.token": null,
 
-		/* Encryption */
-		"encryption.password": null,
+        /* Encryption */
+        "encryption.password": null,
 
-		/* Firebase */
-		"firebase.apiKey": "AIzaSyAp1jrV5OQn9yG-EScMWes827_EvpcQE68",
-		"firebase.authDomain": "zyncco-f9dd0.firebaseapp.com",
-		"firebase.messagingSenderId": "958433754089",
+        /* Firebase */
+        "firebase.apiKey": "AIzaSyAp1jrV5OQn9yG-EScMWes827_EvpcQE68",
+        "firebase.authDomain": "zyncco-f9dd0.firebaseapp.com",
+        "firebase.messagingSenderId": "958433754089",
 
-		/* History */
-		"history": []
-	}, handleError);
+        /* History */
+        "history": []
+    }, handleError);
 }
 
-chrome.storage.local.getBytesInUse(null, result => {
-	if (result === 0) {
-		initializeConfig();
-	}
+
+chrome.storage.local.getBytesInUse(null, function (result) {
+    if (result === 0) {
+        initializeConfig();
+    }
 });
