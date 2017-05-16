da668c642a41f70d8d0ca50be1e8c1767839ed3a
Brandon (Yeti) <branicyeti@live.co.uk>
2017-05-16 12:37:01 +0100

    Possibly support more than just text

--- background/background.js
+++ background/background.js
@@ -1,2 +1,35 @@
-console.log(window.ZyncUtil);
-console.log(window.Zync);
+const CLIPBOARD_CURRENT = document.getElementById("clipboard-current");
+const CLIPBOARD_PREVIOUS = document.getElementById("clipboard-previous");
+const PATTERN_IGNORE = new RegExp("^<!--StartFragment-->.*<!--EndFragment-->$", "im");
+
+CLIPBOARD_CURRENT.addEventListener("paste", function(event) {
+	event.stopPropagation();
+	event.preventDefault();
+	
+	let items = (event.clipboardData || event.originalEvent.clipboardData).items;
+	let data = [];
+	
+	for (let i = 0; i < items.length; i++) {
+		let item = items[0];
+		
+		if (item.kind == "file") {
+			data.push(item.getAsFile());
+		} else {
+			item.getAsString(function(result) {
+				if (!PATTERN_IGNORE.test(result)) {
+					data.push(result);
+				}
+			});
+		}
+	}
+	
+	if (data.length == 0) {
+		console.log("empty");
+		return;
+	}
+	
+	console.log(data)
+});
+
+CLIPBOARD_CURRENT.select();
+document.execCommand('paste');
--- util.js
+++ util.js
@@ -2,4 +2,22 @@ const ZyncUtil = {}
 
 ZyncUtil.isSignedIn = function() {}
 
+ZyncUtil.storageGetAll = function(prefix, callback) {
+	chrome.storage.local.get(null, result => {
+		if (prefix == undefined) {
+			callback(result);
+		} else {
+			let all = {}
+			
+			Object.keys(result).forEach(key => {
+				if (key.startsWith(prefix)) {
+					all[key] = result[key]
+				}
+			});
+			
+			callback(all);
+		}
+	});
+}
+
 window.ZyncUtil = window.ZyncUtil || ZyncUtil;
