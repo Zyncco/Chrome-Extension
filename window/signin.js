b9c5196b6a34e034395ceeb83ed2625dc658c960
Brandon (Yeti) <branicyeti@live.co.uk>
2017-05-17 11:37:22 +0100

    Firebase

--- window/signin.js
+++ window/signin.js
@@ -2,6 +2,12 @@ const PROVIDERS = {
 	google: firebase.auth.GoogleAuthProvider
 }
 
+firebase.initializeApp({
+	"apiKey": "AIzaSyAp1jrV5OQn9yG-EScMWes827_EvpcQE68",
+	"authDomain": "zyncco-f9dd0.firebaseapp.com",
+	"messagingSenderId": "958433754089"
+});
+
 /**
  * Assign click listener to signin button, and go through signin flow.
  * Skeleton function, to be fleshed out.
