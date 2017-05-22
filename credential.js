0b44a29819349ec0ca4c25d19648d58cd7588368
mattrick <git@mattrick.me>
2017-05-21 20:26:48 -0700

    Add popup "breathing" animation; various other improvements

--- credential.js
+++ credential.js
@@ -1,4 +1,4 @@
-var config = {
+const config = {
     apiKey: "AIzaSyAp1jrV5OQn9yG-EScMWes827_EvpcQE68",
     authDomain: "zyncco-f9dd0.firebaseapp.com",
     databaseURL: "https://zyncco-f9dd0.firebaseio.com",
--- util.js
+++ util.js
@@ -1,15 +1,15 @@
-const ZyncUtil = {}
+const ZyncUtil = {};
 
-ZyncUtil.isSignedIn = function() {}
+ZyncUtil.isSignedIn = function() {};
 
 ZyncUtil.getAsset = function(path) {
 	return chrome.runtime.getUrl("/assets/" + path);
-}
+};
 
 ZyncUtil.handleCallbackError = function() {
 	if (chrome.runtime.lastError) {
 		console.error(chrome.runtime.lastError.message);
 	}
-}
+};
 
 window.ZyncUtil = window.ZyncUtil || ZyncUtil;
