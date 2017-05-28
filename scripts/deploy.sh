baf33d43dba5dc933b094050ec74b883d013229c
Amir Ali Omidi <amir@aaomidi.com>
2017-05-28 16:32:21 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 if [ "$TRAVIS_BRANCH" == "amir" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
+	npm install -g yarn
 	npm install -g chrome-webstore-upload-cli
 
 	sed -i 's/"version": "\([0-9]\+\.[0-9]\+\.\)[0-9]\+",/"version": "\1'"$TRAVIS_BUILD_NUMBER"'",/' manifest.json
