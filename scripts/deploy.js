77910a498cb905a5dda9cfe2ad74b664cba8e03c
Brandon (Yeti) <branicyeti@live.co.uk>
2017-06-01 15:03:23 +0100

    Switch from Bower to Yarn
    Working on a sample deployment script (in Javascript, woohoo) that will
    package the extension, upload it to the Chrome webstore and publish
    the extension as well. Travis will only have to run 'yarn run deploy
    [steps]' rather than using a multitude of packages and configurations.

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+const URL_REFRESH_TOKEN = ""
+const URL_WEBSTORE_UPLOAD = ""
+
+function packExtension() {
+	setTimeout(() => {
+		console.log("packExtension");
+	}, 5000);
+}
+
+function uploadExtension() {
+	setTimeout(() => {
+		console.log("uploadExtension");
+	}, 5000);
+}
+
+function publishExtension() {
+	setTimeout(() => {
+		console.log("publishExtension");
+	}, 5000);
+}
+
+function invalidFunction() {
+	console.log("invalidFunction");
+}
+
+let args = process.argv.slice(2);
+for (let i = 0; i < args.length; i++)
+{
+	(window[args[i] + 'Extension'] || invalidFunction)();
+}
