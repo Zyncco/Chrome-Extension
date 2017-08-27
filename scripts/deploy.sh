fefc452dd6171118d6d1e4df3c2ce742470343e0
Vilsol <me@vil.so>
2017-08-27 08:14:36 +0300

    *sigh*

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ npm install -g chrome-webstore-upload-cli
 
 sed -i 's/"version": "\([0-9]\+\.[0-9]\+\.\)[0-9]\+",/"version": "\1'"$TRAVIS_BUILD_NUMBER"'",/' manifest.json
 
-webstore upload --extension-id $EXTENSION_ID --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --refresh-token $REFRESH_TOKEN --auto-publish
+webstore upload --extension-id $EXTENSION_ID --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --refresh-token $REFRESH_TOKEN --source "./packages/zync-1.0.0-chrome.zip" --auto-publish
