3625ef1a5d36dfe3b17b72a47d40bf39828e59e0
Vilsol <me@vil.so>
2017-08-27 07:59:22 +0300

    Deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm install -g chrome-webstore-upload-cli
+
+sed -i 's/"version": "\([0-9]\+\.[0-9]\+\.\)[0-9]\+",/"version": "\1'"$TRAVIS_BUILD_NUMBER"'",/' manifest.json
+
+webstore upload --extension-id $EXTENSION_ID --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --refresh-token $REFRESH_TOKEN --auto-publish
